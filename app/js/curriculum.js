8ba89b72946b81f1a4afed67670d16c2c5e52ff3
lk <lk@lktekiMacBook-Air.local>
2018-11-01 17:09:59 +0800

    游戏界面

--- app/js/curriculum.js
+++ app/js/curriculum.js
@@ -1,41 +1,21 @@
-function goRight(){
-	alert(1)
-}
-window.onload=function(){
-	 //导入editor(初始化)
-	 var editor = ace.edit("editor");//初始化
-	 editor.setTheme("ace/theme/monokai");//设置主题
-	 editor.setFontSize(16);//设置字体大小
-	 editor.setOption("wrap", "free")//自动换行
-	 editor.focus();//获取焦点
-	 enableBasicAutocompletion = true//语法提示
-	 enableLiveAutocompletion = true
-	 editor.moveCursorTo(1, 0);//设置光标在第一行开始
-	 editor.getSession().setMode("ace/mode/python");
-	 editor.session.setMode("ace/mode/python");//设置语言
-	 editor.setOptions({//代码提示
-		 enableBasicAutocompletion: true,
-		 enableSnippets: true,
-		 enableLiveAutocompletion: true
-	 });
 
-	function goRight(){
-		alert(1)
-	}
     var can = document.getElementById("canvas");
 	var tips=document.querySelector(".tips").children[1];
-	// var error=document.querySelector(".error").children[1]
+	var error=document.querySelector(".error");
+	
 	var msg = document.getElementById("msg");
-	var editor_button=document.querySelector(".editor_button");
 	var cxt = can.getContext("2d");
 	var w = 35,h = 35;
 	var editorValue = '';
+	var editor
 	var curMap;//当前的地图
 	var curLevel;//当前等级的地图
 	var curMan;//初始化小人
 	var iCurlevel = 0;//关卡数
 	var moveTimes = 0;//移动了多少次
-	var pushNum = 0
+	var pushNum = 0;
+	var editor;
+	var cursorLocation;
 	//预加载所有图片
 	var oImgs = {
 		"block" : "images/game/block.gif",
@@ -48,27 +28,112 @@ window.onload=function(){
 		"right" : "images/game/right.png",
 	}
 
-	//初始化编辑器
-	var flag=1;
-	// console.log(flag)
-	var editor = ace.edit("editor");//初始化
-		editor.setTheme("ace/theme/monokai");//设置主题
-		editor.setFontSize(16);//设置字体大小
-		editor.setOption("wrap", "free")//自动换行
-		editor.focus();//获取焦点
-		enableBasicAutocompletion = true//语法提示
-		enableLiveAutocompletion = true
-		editor.moveCursorTo(flag, 0);//设置光标在第一行开始
-		editor.getSession().setMode("ace/mode/python");
-		editor.session.setMode("ace/mode/python");//设置语言
-		editor.setOptions({//代码提示
-			enableBasicAutocompletion: true,
-			enableSnippets: true,
-			enableLiveAutocompletion: true
+	$(".curriculum-right-center-left-button-lefe").click(function(){
+		cursorLocation=editor.selection.getCursor().column;//获取光标所在列数
+		if(cursorLocation!=0){//如果光标当前不在第一列，则先换行，否则就在光标所在处追加
+			editor.insert("\r");
+		}
+		editor.insert("IronMan.goLeft");
+		goLeft();
+	});
+	
+	$(".curriculum-right-center-left-button-right").click(function(){
+		cursorLocation=editor.selection.getCursor().column;//获取光标所在列数
+		if(cursorLocation!=0){//如果光标当前不在第一列，则先换行，否则就在光标所在处追加
+			editor.insert("\r");
+		}
+		editor.insert("IronMan.goRight");
+		goRight();
+
+	});
+	$(".curriculum-right-center-left-button-up").click(function(){
+		cursorLocation=editor.selection.getCursor().column;//获取光标所在列数
+		if(cursorLocation!=0){//如果光标当前不在第一列，则先换行，否则就在光标所在处追加
+			editor.insert("\r");
+		}
+		editor.insert("IronMan.goUp");
+		goUp();
+
+	});
+	$(".curriculum-right-center-left-button-down").click(function(){
+		cursorLocation=editor.selection.getCursor().column;//获取光标所在列数
+		if(cursorLocation!=0){//如果光标当前不在第一列，则先换行，否则就在光标所在处追加
+			editor.insert("\r");
+		}
+		editor.insert("IronMan.goDown");
+		goDown();
+
+	});
+
+		$("#editor").bind("keydown",function(e){
+		// 兼容FF和IE和Opera
+			var theEvent = e || window.event;
+			var code = theEvent.keyCode || theEvent.which || theEvent.charCode;
+			if (code == 13) {	
+				data = 	editor.getValue();
+				// var str = "abcdaabbssaaa";
+				// var reg = new RegExp("a","g");
+				// var a = str.replace(reg,"");
+				// console.log(a);
+
+				var left = new RegExp("IronMan.goLeft","g");
+				data = data.replace(left, '');
+				var up = new RegExp("IronMan.goUp","g");
+				data = data.replace(up, '');
+				var down = new RegExp("IronMan.goDown","g");
+				data = data.replace(down, '');
+				var right = new RegExp("IronMan.goRight","g");
+				data = data.replace(right, '');
+				console.log(data);
+				$.ajax({
+					url: 'http://47.100.31.94:5000/check_code',      
+					datatype: "json",      
+					type: 'post',    
+     				 data:{
+			        		id: 'formdata',
+			        		text:data
+			        },
+					success: function (data) {  
+						console.log(data);
+						// alert(data.length);
+						//成功后回调      
+						if(data[0]!="null"){//有错误或者警告的时候
+							for(let i=0;i<data.length;i++){
+								if(data[i]!=null){
+									console.log(data[i]);
+									if(data[i].code.substr(0,1)=="E"){
+									var erroeline=data[i].line;
+									console.log(erroeline);
+									var oldstyle=$("#editor").children("div").eq(0).children("div").eq(0).children("div").eq(erroeline-1);
+									oldstyle.removeAttr("class"); 
+									oldstyle.attr("class", " ace_error");
+									oldstyle.addClass("ace_gutter-cell");
+								}
+								else if(data[i].code.substr(0,1)=="W"){
+									var erroeline=data[i].line;
+									console.log(erroeline);
+									var oldstyle=$("#editor").children("div").eq(0).children("div").eq(0).children("div").eq(erroeline-1);
+									oldstyle.removeAttr("class"); 
+									oldstyle.attr("class", " ace_warning");
+									oldstyle.addClass("ace_gutter-cell");
+								}
+							}
+						}
+						}else{//没有错误或者没有警告
+							console.log("OK");
+						}		   
+					},      
+					error: function(e){      
+						//失败后回调      
+						// alert("服务器请求失败");      
+					},      
+					beforeSend: function(){      
+						//发送请求前调用，可以放一些"正在加载"之类额话      
+						// alert("正在加载");           
+					}
+			});
+		}
 		});
-		editor.on("change", function(e){
-			　　editor.execCommand("startAutocomplete");
-		})
 
 
 	function imgPreload(srcs,callback){
@@ -164,7 +229,7 @@ window.onload=function(){
 	//初始化游戏等级
 	function initLevel(){
 		curMap = copyArray(levels[iCurlevel]);//当前移动过的游戏地图
-		// console.log(curMap);
+		console.log(curMap);
 		curLevel = levels[iCurlevel];//当前等级的初始地图
 		curMan = down;//初始化小人
 		InitMap();//初始化地板
@@ -218,7 +283,7 @@ window.onload=function(){
 		//若果小人能够移动的话，更新游戏数据，并重绘地图
 		if(Trygo1(p1,p2)){
 			moveTimes ++;
-			showMoveInfo();
+			// showMoveInfo();
 		}
 			
 		
@@ -303,7 +368,7 @@ window.onload=function(){
 		if(p1.y<0) return false;//若果超出地图的左边，不通过
 		if(p1.x>curMap.length) return false;//若果超出地图的下边，不通过
 		if(p1.y>curMap[0].length) return false;//若果超出地图的右边，不通过
-		// if(curMap[p1.x][p1.y]==1){error.innerHTML="error:前方已无路";return false; }//若果前面是墙，不通过
+		if(curMap[p1.x][p1.y]==1){error.innerHTML="小朋友，前方没有路了哦";return false; }//若果前面是墙，不通过
 		if(curMap[p1.x][p1.y]==3){error.innerHTML="点击推箱子";return false}//当人物前面是箱子，则需要点击推箱子
 		if (curMap[p1.x][p1.y]==3 || curMap[p1.x][p1.y]==5)//若果小人前面是箱子那就还需要判断箱子前面有没有障碍物(箱子/墙)
 		//1-墙壁，2-陷阱，3-箱子，4-人物，5-箱子及陷阱位置
@@ -312,7 +377,7 @@ window.onload=function(){
 			if (curMap[p2.x][p2.y]==1 || curMap[p2.x][p2.y]==3)
 			{
 				// alert("前方已无路")
-				error.innerHTML="error:前方已无路";
+				error.innerHTML="小朋友，前方没有路了哦";
 				return false;
 			} 
 			//若果判断不成功小人前面的箱子前进一步
@@ -336,8 +401,10 @@ window.onload=function(){
 		curMap[perPosition.x][perPosition.y] = v;
 		//若果判断小人前进了一步，更新坐标值
 		perPosition = p1;
+		error.innerHTML="";
 		//若果小动了 返回true 指代能够移动小人
 		return true;
+
 	}
 
 		function Trygo1(p1,p2){
@@ -346,7 +413,7 @@ window.onload=function(){
 		if(p1.y<0) return false;//若果超出地图的左边，不通过
 		if(p1.x>curMap.length) return false;//若果超出地图的下边，不通过
 		if(p1.y>curMap[0].length) return false;//若果超出地图的右边，不通过
-		if(curMap[p1.x][p1.y]==1){error.innerHTML="error:前方已无路";return false; }//若果前面是墙，不通过
+		if(curMap[p1.x][p1.y]==1){error.innerHTML="小朋友，前方没有路了哦";return false; }//若果前面是墙，不通过
 		//if(curMap[p1.x][p1.y]==3){alert("点击推箱子");return false}//当人物前面是箱子，则需要点击推箱子
 		if (curMap[p1.x][p1.y]==3 || curMap[p1.x][p1.y]==5)//若果小人前面是箱子那就还需要判断箱子前面有没有障碍物(箱子/墙)
 		//1-墙壁，2-陷阱，3-箱子，4-人物，5-箱子及陷阱位置
@@ -355,7 +422,7 @@ window.onload=function(){
 			if (curMap[p2.x][p2.y]==1 || curMap[p2.x][p2.y]==3)
 			{
 				// alert("前方已无路")
-				error.innerHTML="error:前方已无路";
+				error.innerHTML="小朋友，前方没有路了哦";
 				return false;
 			} 
 			//若果判断不成功小人前面的箱子前进一步
@@ -382,17 +449,32 @@ window.onload=function(){
 		//若果小动了 返回true 指代能够移动小人
 		return true;
 	}
-	
+	//判断是否推成功
+	//与键盘上的上下左右键关联
+	//与按钮结合
+	// function doKeyDown(event){
+	// 	switch (event.keyCode)
+	// 	{
+	// 	case 37://左键头
+	// 		go("left");
+	// 		break;
+	// 	case 38://上键头
+	// 		go("up");
+	// 		break;
+	// 	case 39://右箭头
+	// 		go("right");
+	// 		break;
+	// 	case 40://下箭头
+	// 		go("down");
+	// 		break;
+	// 	}
+
+	// }
 	//点击事件
-	var editorText=document.getElementById('editor').innerText;//代码框的数据
-	// console.log(editorText)
 	function goDown(){
-		var name=arguments.callee.name+"()";
-		editor.insert(name);//在光标出插入值
-		editor.focus();//初始化焦点
-		// editor.insert(name);
-		// editor.moveCursorTo(1, 0);
+		// console.log(1)
 		go("down");
+		// tips.innerHTML+=arguments.callee.name+"()"+"<br>";
 		trycatch()
 	}
 
@@ -401,25 +483,26 @@ window.onload=function(){
 		for(var i=0;i<4;i++){
 			go("down");
 		}
+		// tips.innerHTML+=arguments.callee.name+"()"+"<br>";
 		trycatch()
 	}
 	function goUp(){
 		go("up");
 		trycatch();
+		// tips.innerHTML+=arguments.callee.name+"()"+"<br>"
 	}
 	function goLeft(){
 		go("left");
 		trycatch()
+		// tips.innerHTML+=arguments.callee.name+"()"+"<br>"
 	}
 	function goRighttwo(){
 		for(var i=0;i<2;i++){
 			go("right");
 		}
 		trycatch()
+		// tips.innerHTML+=arguments.callee.name+"()"+"<br>"
 	}
-	$(".curriculum-right-center-left-button-lefe").click(function(){
-		go("right")
-	})
 	function goRightthree(){
 		for(var i=0;i<3;i++){
 			go("right");
@@ -429,15 +512,7 @@ window.onload=function(){
 	}
 
 	function goRight(){
-		flag++;
-		console.log(editor.session.getLength())
-		var name=arguments.callee.name+"()";
-		editor.insert(name);//在光标出插入值
-		editor.focus();//初始化焦点
-		// editor.gotoLine(editor.session.getLength()++);
-		editor_button.onclick=function(){
-			go("right");
-		}
+		go("right");
 		trycatch()
 		// tips.innerHTML+=arguments.callee.name+"()"+"<br>"
 	}
@@ -480,6 +555,7 @@ window.onload=function(){
 		// return true;
 		//获取当前坐标的前后左右是否有箱子
 	}
+	document.querySelector
 
 
 	//重玩本关
@@ -498,6 +574,10 @@ window.onload=function(){
 		}
 	}
 
+	function popContent(){
+		alert(editor.getValue());
+	}
+
 
 	//完善关卡数据及游戏说明
 	// function showMoveInfo(){
@@ -511,7 +591,7 @@ window.onload=function(){
 		{
 			msg.innerHTML = "根据提示，把箱子全部推到小球的位置即可过关。箱子只可向前推，不能往后拉，并且小人一次只能推动一个箱子。";
 		}else{
-			showMoveInfo();
+			// showMoveInfo();
 		}
 	}
 
@@ -524,5 +604,3 @@ window.onload=function(){
 		}
 		return b;
 	}
-
-}
