779e52d4caafe54128e50402f050881984e8f34f
lk <lk@lktekiMacBook-Air.local>
2018-11-15 15:14:18 +0800

    下拉框

--- app/js/infocompelete.js
+++ app/js/infocompelete.js
@@ -1,22 +1,22 @@
 $(function(){
-     // Pretty simple huh?
-    //  var scene = document.getElementById('scene');
-    //  var parallax = new Parallax(scene);
+    //  Pretty simple huh?
+     var scene = document.getElementById('scene');
+     var parallax = new Parallax(scene);
 
-    //  //初始化时间选择器
-    //  zaneDate({
-    //      elem: '#zane-calendar',
-    //      width: 300,
-    //      // 插件宽度配置 250 <= X <= 500
-    //      height: 300,
-    //      // 插件高度配置 250 <= X <= 350
-    //      behindTop: 0,
-    //  })
+     //初始化时间选择器
+     zaneDate({
+         elem: '#zane-calendar',
+         width: 300,
+         // 插件宽度配置 250 <= X <= 500
+         height: 300,
+         // 插件高度配置 250 <= X <= 350
+         behindTop: 0,
+     })
      
-    //  //初始化地址选择器
-    //  ylc = yeluochenCity('info_city', (city) => {console.log(city)}, null);
-    // //  console.log(ylc)
-    // //  
+     //初始化地址选择器
+     ylc = yeluochenCity('info_city', (city) => {console.log(city)}, null);
+    //  console.log(ylc)
+    //  
 
     $(".register-success>p").click(function(){
         var token=JSON.parse(localStorage.getItem('token'));
